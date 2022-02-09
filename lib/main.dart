08b18fef2bc0171c87e17b916acf0badafce1050
Ali ijaz <Se.Aliijaz@gmail.com>
2022-02-10 00:20:44 +0500

    till quotes data
    Having error in Line number 16
    quote_data.dart
    
    final response = await http.get('https://favqs.com/api/qotd');

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:quote_app/home_screen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import './quote_data.dart';
+import './favorite_quotes.dart';
 
-class MyApp extends StatelessWidget {
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: DefaultTabController(
+          length: 2,
+          child: HomePage(),
+        ),
+      ),
+    );
+
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      ///Title of app
-      title: 'Quotes-App',
-
-      ///DebugShowCheckedModeBanner
-      debugShowCheckedModeBanner: false,
-
-      ///home
-      home: HomeScreen(),
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          'Daily motivation',
+          style: TextStyle(
+            fontSize: 22.0,
+          ),
+        ),
+        bottom: TabBar(
+          tabs: [
+            Tooltip(
+              message: 'Daily Quotes',
+              child: Tab(
+                icon: Icon(
+                  Icons.today,
+                ),
+              ),
+            ),
+            Tab(
+              icon: Icon(Icons.favorite),
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        children: <Widget>[
+          Stack(children: <Widget>[
+            Center(
+              child: Image.asset(
+                'images/background.jpg',
+                width: size.width,
+                height: size.height,
+                fit: BoxFit.fill,
+              ),
+            ),
+            QuoteData(),
+          ]),
+          FavoriteQuotes(),
+        ],
+      ),
     );
   }
 }
