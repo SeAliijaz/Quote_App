47a721b4e2f383130e5d63d337a4f1f58155fa05
Ali ijaz <Se.Aliijaz@gmail.com>
2022-02-25 03:33:12 +0500

    UI & user can copy and share

---
+++ lib/Constants/constants.dart
@@ -0,0 +1,6 @@
+import 'package:fluttertoast/fluttertoast.dart';
+
+///FlutterToast
+void showToastMsg(String msg) {
+  Fluttertoast.showToast(msg: msg);
+}
--- lib/Home_Screen/home_screen.dart
+++ lib/Home_Screen/home_screen.dart
@@ -1,54 +1,124 @@
-import 'package:flutter/material.dart';
-import 'package:quote_app/Components/favorite_quotes.dart';
-import 'package:quote_app/Data/quote_data.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+import 'package:clipboard/clipboard.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quote_app/Constants/constants.dart';
+import 'package:quote_app/Models/quote_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  ///API_Link
+  String linkAPI = 'https://favqs.com/api/qotd';
+
+  ///Method
+  Future<Quote> fetchData() async {
+    Quote quoteModel;
+    http.Response response = await http.get(Uri.parse(linkAPI));
+    Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+    quoteModel = Quote.fromJson(jsonResponse);
+    return quoteModel;
+  }
+
+  ///Init State
+  @override
+  void initState() {
+    fetchData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Daily motivation',
-            style: TextStyle(
-              fontSize: 22.0,
-            ),
-          ),
-          bottom: TabBar(
-            tabs: [
-              Tooltip(
-                message: 'Daily Quotes',
-                child: Tab(
-                  icon: Icon(
-                    Icons.today,
-                  ),
-                ),
-              ),
-              Tab(
-                icon: Icon(Icons.favorite),
-              ),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Daily motivation Quotes',
         ),
-        body: TabBarView(
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(
-                    'images/bgg.jpg',
+      ),
+      body: Container(
+        height: size.height,
+        width: size.width,
+        child: FutureBuilder<Quote>(
+          future: fetchData(),
+          builder: (BuildContext context, AsyncSnapshot<Quote> snapshot) {
+            if (!snapshot.hasData) {
+              return Center(child: CircularProgressIndicator());
+            }
+            final v = snapshot.data;
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Divider(),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: size.width,
+                    child: Center(
+                      child: Column(
+                        children: [
+                          Text(
+                            "${v.quoteText}",
+                            style: GoogleFonts.raleway(
+                              textStyle: TextStyle(
+                                fontSize: 30,
+                              ),
+                            ),
+                          ),
+                          Divider(),
+                          Container(
+                            width: size.width,
+                            child: Center(
+                              child: Text(
+                                '-${v.quoteAuthor}-',
+                                style: GoogleFonts.raleway(
+                                  textStyle: TextStyle(
+                                    fontSize: 30,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Divider(),
+                        ],
+                      ),
+                    ),
                   ),
-                  fit: BoxFit.fill,
                 ),
-              ),
-              child: Center(
-                child: QuoteData(),
-              ),
-            ),
-            FavoriteQuotes(),
-          ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Share.share("${v.quoteText} - ${v.quoteAuthor}").then(
+                          (value) =>
+                              showToastMsg('Sharing your Quote Successfully!'),
+                        );
+                      },
+                      icon: Icon(Icons.share),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        FlutterClipboard.copy(
+                                "${v.quoteText} - ${v.quoteAuthor}")
+                            .then(
+                          (value) => showToastMsg('Copied Quote Successfully!'),
+                        );
+                      },
+                      icon: Icon(Icons.copy),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
